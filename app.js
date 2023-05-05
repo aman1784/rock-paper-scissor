6605b947c9d63118b435d82effe01715f1d06e27
Aman Seth <68745555+aman1784@users.noreply.github.com>
2023-05-06 01:25:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,69 @@
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
+
+let userChoice;
+let computerChoice;
+let res;
+
+const possibleChoices = document.querySelectorAll('button')
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice =  e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice();
+    getResult();
+}));
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1 //or you can use "possibleChoices.length" in place of "3"
+    // console.log(randomNumber);
+    if(randomNumber === 1){
+        computerChoice = 'Rock'
+    }
+    else if(randomNumber === 2){
+        computerChoice = 'Paper'
+    }
+    else{
+        computerChoice = 'Scissor'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice;
+    return;
+}
+
+
+function getResult(){
+    if(userChoice === computerChoice){
+        resultDisplay.innerHTML = 'Tie :) :(';
+        resultDisplay.style.color = 'White';
+    }
+
+    else if( (userChoice === 'Rock') && (computerChoice === 'Paper') ){
+        resultDisplay.innerHTML = 'You Lost :(';
+        resultDisplay.style.color = 'red';
+    }
+    else if((userChoice === 'Rock') && (computerChoice === 'Scissor') ){
+        resultDisplay.innerHTML = 'You Win :)';
+        resultDisplay.style.color = 'green';
+        
+    }
+    else if((userChoice === 'Paper') && (computerChoice === 'Scissor') ){
+        resultDisplay.innerHTML = 'You Lost :(';
+        resultDisplay.style.color = 'red';
+    }
+
+    else if((computerChoice === 'Rock') && (userChoice === 'Paper') ){
+        resultDisplay.innerHTML = 'You Win :)';
+        resultDisplay.style.color = 'green';
+        
+    }
+    else if((computerChoice === 'Rock') && (userChoice === 'Scissor') ){
+        resultDisplay.innerHTML = 'You Lost :(';
+        resultDisplay.style.color = 'red';
+    }
+    else if((computerChoice === 'Paper') && (userChoice === 'Scissor') ){
+        resultDisplay.innerHTML = 'You Win :)';
+        resultDisplay.style.color = 'green';
+        
+    }
+}
